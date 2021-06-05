3aaab1dcdf41bdc61620fd802c728df63e9b5126
leeladher <leeladher.aj.63@gmail.com>
2021-06-05 19:41:40 +0530

    addfullPath

--- Users/LEELADHER/AndroidStudioProjects/UploadVideo/app/src/main/java/com/leeladher/uploadvideo/MainActivity.kt
+++ Users/LEELADHER/AndroidStudioProjects/UploadVideo/app/src/main/java/com/leeladher/uploadvideo/MainActivity.kt
@@ -2,11 +2,15 @@ package com.leeladher.uploadvideo
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.MediaController
 import android.widget.VideoView
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
@@ -49,13 +53,21 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == 8) {
-            if (requestCode == 8 && resultCode == RESULT_OK) {
-                var videouri = data!!.data
-                vv.setVideoURI(videouri)
+            if (data?.data != null) {
+                var uri:Uri = data.data!!
+                vv.setVideoURI(uri)
+                var mediaController:MediaController = MediaController(this)
+                vv.setMediaController(mediaController)
+                vv.start()
+
             }
         }
     }
+
+
 }
+
